402729593d475d9eed06c68d9ae0c01a46eaf480
manoharm <manoharm@DESKTOP-8D8LED0>
2022-10-06 18:42:39 +0530

    Commit by manohar

--- CRUDEMOVIESWEBAPI/Model/Actor.cs
+++ CRUDEMOVIESWEBAPI/Model/Actor.cs
@@ -1,16 +1,11 @@
 ﻿namespace CRUDEMOVIESWEBAPI.Model
 {
-    public class Actor
+    public class Actor: BaseModelClass
     {
         //aId,aName,aGender,createdBy,createdDate,modifiedBy,modifiedDate,isDeleted
         public int aId { get; set; }
         public string aName { get; set; }
         public string aGender { get; set; }
-        public int createdBy { get; set; }
-        public DateTime createdDate { get; set; }
-        public int modifiedBy { get; set; }
-        public  DateTime modifiedDate { get; set; }
-        public int isDeleted { get; set; }
         public Movie movie{ get; set; } 
 
     }
